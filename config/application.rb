5db911eb9fe472cab58a7265ab785c6e584ee911
TsubasaYoshida <tsubasa.yoshida.business@gmail.com>
2020-10-10 20:19:53 +0900

    ジェネレータをカスタマイズする

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,23 @@ module ShortHair
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    # Don't generate system test files.
-    config.generators.system_tests = nil
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.test_framework :rspec,
+                       # テストデータベースにレコードを作成するファイルの作成をスキップ
+                       fixtures: false,
+                       # ビュースペックの作成をスキップ(UIのテストはフィーチャスペックに任せる)
+                       view_specs: false,
+                       # ヘルパースペックの作成をスキップ
+                       helper_specs: false,
+                       # ルーティングスペックの作成をスキップ
+                       routing_specs: false,
+                       # コントローラースペックの作成をスキップ
+                       controller_specs: false,
+                       # リクエストスペックの作成をスキップ
+                       request_specs: false
+
+    end
   end
 end
